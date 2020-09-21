1b843e920f8f24b1b67e2e322f2a4d7fa9870356
Dipo Rodipe <dipo106@gmail.com>
2020-09-21 14:03:15 +0100

    added objective-03

--- Tasks/Objective-02.py
+++ Tasks/Objective-02.py
@@ -4,7 +4,8 @@
 #Pick at least one from the EASY, MEDIUM and HARD level tasks below:
 
 ##[EASY] Simple adder
-#write a program that asks the user for two numbers, adds them together and outputs the result. For example, if a user enters 5 and 12, the program will output 12
+#write a program that asks the user for two numbers, adds them together and outputs the result. 
+#For example, if a user enters 5 and 12, the program will output 12
 
 #PUT CODE BELOW HERE:
 
@@ -14,6 +15,65 @@
 
 
 ##[EASY] Test marks
+#Write a program that will ask the user to enter three test marks out of 100 and output the average.
+
+#PUT CODE BELOW HERE:
+
+
+
+
+
+##[EASY] Temperature converter challenge
+#Write a program to enter a temperature in degrees Fahrenheit and display the equivalent temperature in degrees Centigrade.
+#The formula for conversion is Centigrade = (Fahrenheit – 32) * (5/9)
+
+#PUT CODE BELOW HERE:
+
+
+
+
+
+##[MEDIUM] Height & weight
+#Write a program to convert a person’s height in inches into centimetres and their weight in stones into kilograms.  (1 inch = 2.54 cm and 1 stone = 6.364 kg)
+
+#PUT CODE BELOW HERE:
+
+
+
+
+
+
+##[MEDIUM] Toy cars
+#A worker gets paid £12/hour plus £0.60 for every toy car they make in a factory.  Write a program that allows the worker to enter the number of hours they have worked and the number of cars they have made.  The program should output their wages for the day.
+
+#PUT CODE BELOW HERE:
+
+
+
+
+
+
+##[MEDIUM] Fish tank volume challenge
+#Write a program that will ask the user to enter the length, depth and height of a fish tank in cm.  Calculate the volume of water required to fill the tank and display this volume in litres and UK gallons.
+#To calculate volume in litres, multiply length by depth by height and divide by 1000.
+
+#PUT CODE BELOW HERE:
+
+
+
+
+
+
+##[HARD] Circle properties 
+#Write a program that:
+#•	Asks the user to enter the diameter of a circle.
+#•	Asks the user to enter the arc angle.
+#•	Outputs the radius of the circle (diameter divided by 2).
+#•	Outputs the area of the circle (3.14 multiplied by the radius squared).
+#•	Outputs the circumference of the circle (3.14 multiplied by the diameter).
+#•	Outputs the arc length (circumference multiplied by the arc angle, divided by 360).
+
+#PUT CODE BELOW HERE:
 
 
 
---
+++ Tasks/Objective-03.py
@@ -0,0 +1,33 @@
+#Objective-03.py
+
+#Pick at least one from the EASY, MEDIUM and HARD level tasks below:
+
+##[EASY] Under age challenge
+#Write a program that asks for your age.  If you are over 18 it outputs the message, “Over 18”, otherwise it outputs, “Under age”.
+
+#PUT CODE BELOW HERE:
+
+
+
+
+##[EASY] Water temperature challenge
+#Write a program that reads in the temperature of water in a container in Centigrade and displays a message stating whether the water is frozen (zero or below), boiling (100 or greater) or neither.
+
+#PUT CODE BELOW HERE:
+
+
+
+
+
+
+##[EASY] Vocational grade challenge
+#Write a program that allows you to enter a test mark out of 100.
+#The program outputs “FAIL” for a score less than 40, “PASS” for a score of 40 or more, “MERIT” for a score of 60 or more and “DISTINCTION” for a score of 80 or more.
+
+#PUT CODE BELOW HERE:
+
+
+
+
+
+
