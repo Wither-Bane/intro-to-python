017f317ef25e055564f25161f1027b205c4b7b89
Dipo Rodipe <dipo106@gmail.com>
2020-10-19 14:39:31 +0100

    Added additional random module function and a nice-looking input statement to end the program on

--- Examples/04-arithmeticAndRandom.py
+++ Examples/04-arithmeticAndRandom.py
@@ -28,7 +28,7 @@ print(number1,"to the power of",number2,"is",power_of_result)
 
 print(number1,"divided by",number2,"is",division_result) 
 
-print(number1,"divided by",number2,"is",integer_division_result) 
+print(number1,"integer-divided by",number2,"is",integer_division_result) 
 
 print(number1,"divided by",number2,"has a remainder of",modulus_result)
 
@@ -45,6 +45,11 @@ random_number = random.randint(1,6)
 print("You rolled a",random_number) 
 
 
+#Pick a name out of the hat
+
+names = ["pikachu", "Charzard", "Lucario"]
+
+print("For my pokemon, I choose you, "+ random.choice(names) +"!")
 
 
 ### Math Module
@@ -61,3 +66,8 @@ print("2 to the power of 3 = ", ans)
 #Displays the square root of a number
 ansRooted = math.sqrt(ans)
 print("The root of 8, our previous answer, is", ansRooted)
+
+
+
+
+input("\n\n\n------------Press enter to exit------------")
